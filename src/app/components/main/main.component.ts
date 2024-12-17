cad5249db5efcad3754b15c0f3001b320cf5de98
Gabriel <gaby13453@gmail.com>
2024-12-16 21:24:38 -0500

    style: :sparkles: Hero and logo act/map section

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,11 +3,13 @@ import { FooterCenterComponent } from '../footer/footer-center/footer-center.com
 import { HeaderCenterComponent } from '../header/header-center/header-center.component';
 import { HeaderLeftComponent } from "../header/header-left/header-left.component";
 import { HeaderRightComponent } from "../header/header-right/header-right.component";
+import { HeroComponent } from "../pag1-fundacion/hero/hero.component";
+import { MapComponent } from "../pag1-fundacion/map/map.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderCenterComponent, FooterCenterComponent, HeaderLeftComponent, HeaderRightComponent],
+  imports: [HeaderCenterComponent, FooterCenterComponent, HeaderLeftComponent, HeaderRightComponent, HeroComponent, MapComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
